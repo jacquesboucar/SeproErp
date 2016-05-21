a96bda0faabc8d3fcfdcb5321993878231a762ee
Boucar Sagne <tiagoboucar@yahoo.fr>
2016-05-21 11:49:58 +0000

    first commit

---
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+//$version = '3.3.2';
+$prodName = 'Sepro Senegal';
+$copyrightYear = date('Y');
+
+?>
+<?php //echo $prodName;?><br/>
+&copy; <?php echo $copyrightYear;?> <a href="" target="_blank">Sepro Senegal</a>.
