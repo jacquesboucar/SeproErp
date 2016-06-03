64cf8f8af17a1403f1ba55ef45a43b23fee6f226
Rokhaya <kayaahgueye@gmail.com>
2016-06-03 10:19:08 +0000

    Modifications des social links et du Favicon

--- symfony/apps/orangehrm/templates/_footer_copyright_social_links.php
+++ symfony/apps/orangehrm/templates/_footer_copyright_social_links.php
@@ -6,13 +6,13 @@ $imagePath = theme_path("images/login");
     <?php include_partial('global/copyright'); ?>
     </div>
     <div id="social-icons">
-        <a href="http://www.linkedin.com/groups?home=&amp;gid=891077" target="_blank">
-            <img src="<?php echo "{$imagePath}/linkedin.png"; ?>" alt="LinkedIn OrangeHRM group"/></a>&nbsp;
-        <a href="http://www.facebook.com/OrangeHRM" target="_blank">
-            <img src="<?php echo "{$imagePath}/facebook.png"; ?>" alt="OrangeHRM on Facebook"/></a>&nbsp;
-        <a href="http://twitter.com/orangehrm" target="_blank">
-            <img src="<?php echo "{$imagePath}/twiter.png"; ?>" alt="OrangeHRM on twitter"/></a>&nbsp;
-        <a href="http://www.youtube.com/orangehrm" target="_blank">
-            <img src="<?php echo "{$imagePath}/youtube.png"; ?>" alt="OrangeHRM on youtube"/></a>&nbsp;
-    </div>
+        <a href="http://www.facebook.com/SabluxGroup" target="_blank">
+            <img src="<?php echo "{$imagePath}/facebook.png"; ?>" alt="Sablux Group on Facebook"/></a>&nbsp;
+        <a href="http://twitter.com/SabluxGroup" target="_blank">
+            <img src="<?php echo "{$imagePath}/twiter.png"; ?>" alt="Sablux Group on twitter"/></a>&nbsp;
+        <a href="http://www.youtube.com/watch?feature=player_embedded&v=wMBBHTMEpb4" target="_blank">
+            <img src="<?php echo "{$imagePath}/youtube.png"; ?>" alt="Sablux Group on youtube"/></a>&nbsp;
+	 <a href="http://www.linkedin.com" target="_blank">
+            <img src="<?php echo "{$imagePath}/linkedin.png"; ?>" alt="LinkedIn Sablux Group"/></a>&nbsp;   
+ </div>
 </div>
