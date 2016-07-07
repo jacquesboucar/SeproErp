3823723a62ef2861a4fe196b344f20943d52e4f0
BLACK-HAT <black-hat@localhost.localdomain>
2016-07-07 13:26:51 +0000

    Bootstrap Login and Menu

--- symfony/apps/orangehrm/templates/freshorange.php
+++ symfony/apps/orangehrm/templates/freshorange.php
@@ -7,9 +7,13 @@ include_slot('header', get_partial('global/header'));
 
     </head>
     <body>
-      
+
+        <header class="headerlogin">
+
+        </header>
+
         <div id="wrapper">
-            
+
             <div id="branding">
                 <a href="http://www.orangehrm.com/" target="_blank"><img src="<?php echo theme_path('images/logo.png')?>" width="283" height="56" alt="OrangeHRM"/></a>
                 <!--<a href="http://www.orangehrm.com/user-survey-registration.php" class="subscribe" target="_blank"><?php echo __('Join OrangeHRM Community'); ?></a>-->
@@ -25,18 +29,18 @@ include_slot('header', get_partial('global/header'));
 <!--                <a href="#" id="help" class="panelTrigger"><?php echo __("Help & Training"); ?></a>
                 <div id="help-menu" class="panelContainer">
                     <ul>
-                        
+
                         <li><a href="http://www.orangehrm.com/support-plans.php?utm_source=application_support&amp;utm_medium=app_url&amp;utm_campaign=orangeapp" target="_blank"><?php echo __('Support'); ?></a></li>
                         <li><a href="http://www.orangehrm.com/training.php?utm_source=application_traning&amp;utm_medium=app_url&amp;utm_campaign=orangeapp" target="_blank"><?php echo __('Training'); ?></a></li>
                         <li><a href="http://www.orangehrm.com/addon-plans.shtml?utm_source=application_addons&amp;utm_medium=app_url&amp;utm_campaign=orangeapp" target="_blank"><?php echo __('Add-Ons'); ?></a></li>
                         <li><a href="http://www.orangehrm.com/customizations.php?utm_source=application_cus&amp;utm_medium=app_url&amp;utm_campaign=orangeapp" target="_blank"><?php echo __('Customizations'); ?></a></li>
                         <li><a href="http://forum.orangehrm.com/" target="_blank"><?php echo __('Forum'); ?></a></li>
                         <li><a href="http://blog.orangehrm.com/" target="_blank"><?php echo __('Blog'); ?></a></li>
-                        <li><a href="http://sourceforge.net/apps/mantisbt/orangehrm/view_all_bug_page.php" target="_blank"><?php echo __('Bug Tracker'); ?></a></li>                        
+                        <li><a href="http://sourceforge.net/apps/mantisbt/orangehrm/view_all_bug_page.php" target="_blank"><?php echo __('Bug Tracker'); ?></a></li>
                     </ul>
                 </div>-->
-            </div> <!-- branding -->      
-            
+            </div> <!-- branding -->
+
             <?php include_component('core', 'mainMenu'); ?>
 
             <div id="content">
@@ -44,12 +48,17 @@ include_slot('header', get_partial('global/header'));
                   <?php echo $sf_content ?>
 
             </div> <!-- content -->
-          
+
         </div> <!-- wrapper -->
-        
-        <div id="footer">
-            <?php include_partial('global/copyright');?>
-        </div> <!-- footer -->        
-        
-        
+
+        <footer class="footerlayout">
+
+            <div id="footer" class="pull-left">
+                <?php include_partial('global/copyright');?>
+            </div>
+            <!-- footer -->
+        </footer>
+
+
+
 <?php include_slot('footer', get_partial('global/footer'));?>
