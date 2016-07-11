96998ce4459a0410ebca37a697e6acb4fff0594a
Cheikh-Tijani <91ibrahime@gmail.com>
2016-07-11 13:06:10 +0000

    header

--- symfony/apps/orangehrm/templates/_header.php
+++ symfony/apps/orangehrm/templates/_header.php
@@ -22,14 +22,15 @@ $cultureElements = explode('_', $sf_user->getCulture());
         <link href="<?php echo public_path('../../themes/orange/css/bootstrap.min.css')?>" rel="stylesheet" type="text/css"/>
         <link href="<?php echo public_path('../../themes/orange/css/style-login.css')?>" rel="stylesheet" type="text/css"/>
         <link href="<?php echo public_path('../../themes/orange/css/bootstrap-theme.css')?>" rel="stylesheet" type="text/css"/>
-        
+        <link href="<?php echo public_path('../../themes/orange/css/font-awesome.css')?>" rel="stylesheet">
         <!-- Custom CSS files -->
         <link href="<?php echo theme_path('css/main.css')?>" rel="stylesheet" type="text/css"/>
         
         <?php       
         // Library JavaScript files
 
-        echo javascript_include_tag('jquery/jquery-1.7.2.min.js');
+
+        echo javascript_include_tag('jquery/jquery-1.9.1.js');
 
         echo javascript_include_tag('jquery/validate/jquery.validate.js');
         
@@ -41,6 +42,9 @@ $cultureElements = explode('_', $sf_user->getCulture());
         echo javascript_include_tag('jquery/jquery.tipTip.minified.js');
         echo javascript_include_tag('jquery/bootstrap-modal.js');
         echo javascript_include_tag('jquery/jquery.clickoutside.js');
+        echo javascript_include_tag('jquery/bootstrap.js');
+        echo javascript_include_tag('jquery/bootstrap.min.js');
+
 
         // Custom JavaScript files
         echo javascript_include_tag('orangehrm.validate.js');
@@ -56,6 +60,7 @@ $cultureElements = explode('_', $sf_user->getCulture());
         <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->        
+        <![endif]-->
+
 
 
